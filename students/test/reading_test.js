8fa85b7909cc031a47372b1ae6cf9db9ff562b1f
Mugundh <mugundhjb@gmail.com>
2023-09-28 17:15:55 +0530

    id in MongoDB

--- students/test/reading_test.js
+++ students/test/reading_test.js
@@ -13,5 +13,7 @@ describe("Read the data", (done) => {
     it("Find all Jasons", async () => {
         const students = await Student.find({name: 'Jason'})
         console.log(students);
+        // assert(students[0]._id === jason._id) - This wont work, becoz it is enclosed with ObjectId. So, unless we convert it to string, it wont work
+        assert(students[0]._id.toString() === jason._id.toString()) // We need to convert id to string in order to work
     })
 })
